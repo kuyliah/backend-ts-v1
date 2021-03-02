321d25f040979a769c72541cb6c1031299094436
Deary <deary@quantumdigitech.com>
2021-03-02 23:07:55 +0700

    low.v1 - add env / config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import config from './config/keys';
+import 'dotenv/config';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
