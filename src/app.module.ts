ce2f436d8f80df2d7b61a3522356c44a8785b4ca
Deary <deary@quantumdigitech.com>
2021-03-02 23:55:16 +0700

    env.v1 - add exception filter error response Status

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SharedModule } from './shared/shared.module';
 import config from './config/keys';
 import 'dotenv/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
